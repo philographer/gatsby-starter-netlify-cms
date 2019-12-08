5e561b483615391c60ddf6ce3a9fb08f2fa6ffad
hogyun.yu <hogyun.yu@navercorp.com>
2019-12-08 21:36:28 +0900

    Edited column size 6->4

--- src/components/SponserRoll.js
+++ src/components/SponserRoll.js
@@ -16,7 +16,7 @@ class SponserRoll extends React.Component {
           posts.map(({ node: post }, idx) => {
             if (idx < count)
               return (
-                <div className="is-parent column is-6" key={post.id}>
+                <div className="is-parent column is-4" key={post.id}>
                   <article
                     className={`blog-list-item tile is-child box notification`}
                   >
